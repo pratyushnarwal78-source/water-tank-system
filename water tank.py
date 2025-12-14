d1c2ff3a98de49cc19ab7404417215ecc3254480
Pratyush singh <pratyushnarwal78@gmail.com>
2025-12-14 16:52:52 +0530

    Add files via upload

---
+++ water tank.py
@@ -0,0 +1,8 @@
+print("welcome to overflow tan motor system :")
+water = int(input("current water level  " + '\n'+ "l"))
+
+if water >= 500 :
+    print("shutdown the water")
+else :
+    print("continue the system")
+
